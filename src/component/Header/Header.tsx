5e65811a28e6aefa12bf484321d43f8928da4911
Arafat Patwary <farabipsk@gmail.com>
2025-12-12 11:22:00 +0600

    add React Router

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,94 +1,46 @@
-import React, { useState } from "react";
-import { Menu } from "lucide-react";
+import React from 'react'
+import { BrowserRouter,Routes,Route, Link } from 'react-router-dom'
+import Product from '../Product/Product.js';
+import Corporate from '../Home/Corporate.js';
+import Home from '../Home/Home.js';
+import Export from '../Home/Export.js';
 
-const Header: React.FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  //Define Main Navigation
-  const navLinks = [
-    { name: "Home", href: "#" },
-    { name: "Product", href: "#" },
-    { name: "Corporate", href: "#" },
-    { name: "Export", href: "#" },
-    { name: "Outlets", href: "#" },
-    { name: "Halal Investment", href: "#" },
-  ];
-
+function Header() {
   return (
-    <header className="bg-white shadow-md">
-      <nav className="flex items-center justify-between max-w-7xl mx-auto px-4 py-2">
-        <div className="flex items-center space-x-2">
-          <div className="text-xl font-semibold text-green-700 font-serif">
-            <span className="text-sm">Khash </span>Food
-            <span className="text-green 700 ml-1">
-              <svg
-                className="w-4 h-4 inline-block align-top"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M13 10V3L4 14h7v7l9-11h-7z"
-                  className="text-green-500"
-                />
-              </svg>
-            </span>
-          </div>
-        </div>
-     {/* /? Desktop */}
-     <div className="hidden lg:flex items-center space-x-6">
-      {navLinks.map((link) =>(
-        <a 
-        key={link.name}
-        href={link.href}
-        className="text-grey-600 hover:text-700 transition duration-150 font-medium text-sm">
-      {link.name}
-        </a>
-      ))}
 
-     </div>
+<BrowserRouter>
 
-     {/* mobile menu */} 
-     <button 
-     onClick={() => setIsMenuOpen(!isMenuOpen)}
-     className="lg:hidden text-gray-600 hover:text-green-700 p-2 rounded-full hover:bg-gray-100
-     aria-label=Toggle Menu"
-     >
-     <Menu className="w-6 h-6"/>
-     </button>
-        
-      </nav>
+<nav className='flex flex-col sm:flex-row sm:justify-start sm:space-x-8 
+   p-4 bg-gray-900 text-white shadow-lg'>
 
-      {/* conditionally renderd */} 
+  <Link to =""
+  className='py-2 px-4 block text-center sm:text-left hover:bg-indigo-600 
+  rounded-md transition duration-200'
+  >Home</Link>
+ < Link to="product"
+ className='py-2 px-4 block text-center sm:text-left hover:bg-indigo-600 
+  rounded-md transition duration-200'
+ >Product</Link>
+ < Link to="corporate"
+ className='py-2 px-4 block text-center sm:text-left hover:bg-indigo-600 
+  rounded-md transition duration-200'
+ >Corporate</Link>
+ <Link to="export"
+ className='py-2 px-4 block text-center sm:text-left hover:bg-indigo-600 
+  rounded-md transition duration-200'
+ >Export</Link>
+</nav>
 
-      <div className="flex items-center space-x-4">
+ <Routes>
+  <Route path='' element={<Home/>}/>
+   <Route path="/product" element={<Product/>}/>
+   <Route path='corporate' element={<Corporate/>}/>
+   <Route path='export' element={<Export/>}/>
+ </Routes>
 
-      
-      {isMenuOpen && (
-        <div className="lg:hidden bg-gray-50 border-t border-gray-200 py-2">
-          {navLinks.map((link) => (
-            <a
-             key={link.name}
+</BrowserRouter>  
 
-            href={link.href}
-            className="block px-4 py-2 text-sm text-gray-700 hover:bg-green-100 hover:text-green-700"
-            onClick={() => setIsMenuOpen(false)}
-            >
-         {link.name}
-         </a>
-         ))}
-
-        </div>
-        
-      )}
-      </div>
-    </header>
-  );
+);
 };
-
-
 
 export default Header;
---
+++ src/component/Home/Export.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Export() {
+  return (
+    <div>Export
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem voluptatem temporibus ullam ab numquam maxime perferendis laborum repellendus praesentium quisquam?</p>
+    </div>
+  )
+}
+
+export default Export
